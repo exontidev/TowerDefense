e65c17998ff052359a79a4d523f65b7df2afed79
exontidev <83339526+exontidev@users.noreply.github.com>
2024-09-28 12:28:32 +0300

    I BESTOW UPON YOU POWER

--- wife creator 3000/Assets/Scripts/EnemySpawner.cs
+++ wife creator 3000/Assets/Scripts/EnemySpawner.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
+    [SerializeField] private float countdown = 3f;
+    [SerializeField] private float timeBetweenEnemySpawn;
+
+    private int wave;
 
     private void Start()
     {
-        InvokeRepeating("Spawn", 1f, 1f);
+        StartCoroutine(SpawnWave());
     }
 
-    private void Spawn()
+    private IEnumerator SpawnWave()
     {
-        Instantiate(enemy, transform.position, Quaternion.identity, transform);
+        yield return new WaitForSeconds(countdown);
+        for (int i = 0; i < wave; i++)
+        {
+            Instantiate(enemy, transform.position, Quaternion.identity, transform);
+            yield return new WaitForSeconds(timeBetweenEnemySpawn);
+        }
+        wave++;
+        StartCoroutine(SpawnWave());
     }
 }
---
+++ wife creator 3000/Assets/Scripts/Weapon.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float radius = 5f;
+
+    private EnemyMovement[] enemies => FindObjectsByType<EnemyMovement>(FindObjectsSortMode.None);
+    private EnemyMovement closestEnemy;
+
+    private void Start()
+    {
+        InvokeRepeating("FindClosestEnemy", 0, 0.5f);
+    }
+
+    private void Update()
+    {
+        if(target)
+            transform.LookAt(target);
+    }
+
+    private void FindClosestEnemy()
+    {
+        float closestDistance = Mathf.Infinity;
+        closestEnemy = null;
+
+        foreach (EnemyMovement enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < closestDistance)
+            {
+                closestDistance = distanceToEnemy;
+                closestEnemy = enemy;
+            }
+        }
+
+        // If there's an enemy within range, you can set it to target it
+        if (closestDistance <= radius && closestEnemy != null)
+        {
+            this.target = closestEnemy.transform;
+        }
+        else
+        {
+            this.target = null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);  
+    }
+}
